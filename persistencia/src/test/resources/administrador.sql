141c5a6aafe9a92cb4023d3644c774768357d9b9
Jhonatan <jcuribep@uqvirtual.edu.co>
2021-10-16 19:42:50 -0500

    algunos sql hechos(administrador)

--- persistencia/src/test/resources/administrador.sql
+++ persistencia/src/test/resources/administrador.sql
@@ -1,14 +1,10 @@
 
-insert into administrador values ("234", "Juan","juan@gmail.com", "2223", telefonos)
-insert into administrador values ("656", "lili","lili@gmail.com", "6789", telefonos)
-insert into administrador values ("987", "Ana","ana@gmail.com", "2356", telefonos)
-
-//Telefonos administrador
-insert into administrador_telefono values ("234", "31456", "casa");
-insert into administrador_telefono values ("234", "34567", "trabajo");
-
-insert into administrador_telefono values ("656", "3456", "casa");
-insert into administrador_telefono values ("656", "6533", "trabajo");
-
-insert into administrador_telefono values ("987", "4567", "casa");
-insert into administrador_telefono values ("987", "7543", "trabajo");
+insert into administrador(cod_persona,email,nombre,password) values ("2340","juan@gmail.com" ,"Juan", "2223");
+insert into administrador(cod_persona,email,nombre,password) values ("1340","Ana@gmail.com" ,"Ana", "222");
+insert into administrador(cod_persona,email,nombre,password) values ("3340","Maria@gmail.com" ,"Maria", "223");
+insert into administrador_telefono values ("2340", "31456", "casa");
+insert into administrador_telefono values ("2340", "34567", "trabajo");
+insert into administrador_telefono values ("1340", "3456", "casa");
+insert into administrador_telefono values ("1340", "6533", "trabajo");
+insert into administrador_telefono values ("3340", "4567", "casa");
+insert into administrador_telefono values ("3340", "7543", "trabajo");
