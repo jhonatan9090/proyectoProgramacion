4039937dc953e367696f0b3c0f9409959f22314f
Jhonatan <jcuribep@uqvirtual.edu.co>
2021-11-20 21:48:40 -0500

    esto es una prueba de las consultas sql

--- negocio/src/test/java/co/edu/uniquindio/proyectoUnishop/test/UsuarioServicioTest.java
+++ negocio/src/test/java/co/edu/uniquindio/proyectoUnishop/test/UsuarioServicioTest.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.proyectoUnishop.test;
 
 import co.edu.uniquindio.proyectoUnishop.NegocioApplication;
+import co.edu.uniquindio.proyectoUnishop.entidades.Ciudad;
 import co.edu.uniquindio.proyectoUnishop.servicios.UsuarioServicio;
 import co.edu.uniquindio.proyectoUnishop.entidades.Usuario;
 import org.junit.jupiter.api.Assertions;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootTest(classes = NegocioApplication.class)
@@ -20,8 +22,30 @@ public class UsuarioServicioTest {
     private UsuarioServicio usuarioServicio;
 
     @Test
-    public void RegistrarTest(){
+    public void ListarUsuariosTest(){
 
+        Map<String,String> telefonos=new HashMap<>();
+        telefonos.put("casa","321414");
+        telefonos.put("celular","321452514");
+        Usuario u= new Usuario("200", "Oscar", "oscar@gmail.com", "oscar1",telefonos,null);
+        u.setTelefono(telefonos);
+        try {
+
+            usuarioServicio.registrarUsuario(u);
+            List<Usuario> respuesta = usuarioServicio.listarUsuarios();
+            for(Usuario miUsuario:respuesta) {
+                System.out.println(miUsuario);
+            }
+
+            Assertions.assertNotNull(respuesta);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void RegistrarUsuarioTest(){
 
         Map<String,String> telefonos=new HashMap<>();
         telefonos.put("casa","321414");
@@ -37,6 +61,33 @@ public class UsuarioServicioTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Test
+    public void loguearUsuario(){
+
+
+        Map<String,String> telefonos=new HashMap<>();
+        telefonos.put("casa","321414");
+        telefonos.put("celular","321452514");
+        Usuario u= new Usuario("200", "Oscar", "oscar@gmail.com", "oscar1",telefonos,null);
+        u.setTelefono(telefonos);
+        try {
+            Usuario respuesta = usuarioServicio.registrarUsuario(u);
+            Usuario usuarioLogueado=usuarioServicio.loguearUsuario("oscar@gmail.com","oscar1");
+            Assertions.assertNotNull(usuarioLogueado);
+        }catch (Exception e){
+
+
+            Assertions.assertTrue(false, e.getMessage());
+
+
+        }
+
+
+
+
     }
 }
 
